6213f84ad171c9b3ee86e67aff5f2585936c61a8
Xenia <listk98@yandex.ru>
2022-03-14 15:06:22 +0300

    Add Task_1.4

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -13,11 +13,11 @@ int N = Prompt("Введите пятизначное число: ");
 bool Palindrom(string str)
 {
 
-    int lengh = str.Length;
+    int length = str.Length;
 
-    for (int i = 0; i < lengh/2; i++)
+    for (int i = 0; i < length/2; i++)
     {
-        if(str[i] != str[lengh-i-1])
+        if(str[i] != str[length-i-1])
         {
             return false; 
         }
@@ -27,9 +27,9 @@ bool Palindrom(string str)
 
 
 
-if ((N) > 9999 && (N) < 100000)
+if (N > 9999 && N < 100000)
 {
-    string strNew = (N).ToString();
+    string strNew = N.ToString();
     if(Palindrom(strNew)) 
     {
         Console.WriteLine("Палиндром");
