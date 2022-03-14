71bc26750afe5a9d21d516a1a0cd1fe9f2910bd7
Xenia <listk98@yandex.ru>
2022-03-14 14:18:49 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿int Prompt(String message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int N = Prompt("Введите пятизначное число: ");
+
+bool Polindrom(string str)
+{
+
+    int lengh = str.Length;
+    
+    for (int i = 0; i < lengh/2; i++)
+    {
+        if(str[i] != str[lengh-i-1])
+        {
+            return false; 
+        }
+    }
+    return true;
+}
+
+if (N > 9999 && N < 100000)
+{
+    string strNew = N.ToString();
+    if(Polindrom(strNew)) 
+    {
+        Console.WriteLine("Полиндром");
+    }
+    else Console.WriteLine("Не полиндром");
+}
+
+else Console.WriteLine("Число не является пятизначным");
+
