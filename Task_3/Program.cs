74b06f87f470f26d3d4c83b6330b3cab34265ab3
Xenia <listk98@yandex.ru>
2022-03-15 22:53:01 +0300

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+Console.Write("Введите натуральное число N: ");
+string numberN = Console.ReadLine();
+int N = int.Parse(numberN);
+
+if (N > 0)
+{
+	for (int i = 1; i <= N; i++)
+	{
+		Console.Write($"{Math.Pow(i,3)} ");
+	}
+}
+else Console.WriteLine($"Введено некорректное значение. Число {N} не является натуральным");
+
+
+
